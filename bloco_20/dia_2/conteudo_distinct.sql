d524d47ae4516e75bbe2d29b21221280b710d033
andy silva <andssilva.23@gmail.com>
2021-07-25 07:44:11 -0400

    feat: :rocket: content: DISTINCT

---
+++ bloco_20/dia_2/conteudo_distinct.sql
@@ -0,0 +1,10 @@
+SELECT * FROM Escola.Alunos;
+
+-- Monte uma query para encontrar pares únicos de nomes e idades .
+SELECT DISTINCT * FROM Escola.Alunos;
+
+-- Monte uma query para encontrar somente os nomes únicos.
+SELECT DISTINCT Nome FROM Escola.Alunos;
+
+-- Monte uma query para encontrar somente as idades únicas.
+SELECT DISTINCT Idade FROM Escola.Alunos;
