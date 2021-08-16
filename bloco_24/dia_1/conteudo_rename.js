4d60d6eab9f62abca66fab3b08ba584f2ab157b3
andy silva <andssilva.23@gmail.com>
2021-08-16 17:55:57 -0400

    feat: :rocket: content:

---
+++ bloco_24/dia_1/conteudo_rename.js
@@ -0,0 +1,16 @@
+use("conteudo_trybe")
+/* db.fruits.insertOne(
+  { _id: 100, name: "Banana", quantity: 100, inStock: true }
+);
+*/
+
+db.fruits.find();
+
+/* db.fruits.updateOne(
+  { name: "Banana" },
+  { $rename: {
+      "name": "productName"
+    }
+  }
+);
+*/
