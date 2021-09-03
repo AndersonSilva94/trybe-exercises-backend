a2967ba5863984c4527504241f13b2fa7b762f1d
andy silva <andssilva.23@gmail.com>
2021-09-03 15:49:12 -0400

    feat: :rocket: exercises: exercise 1

---
+++ bloco_26/dia_4/exercicios/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(3001, () => {
+  console.log('Aplicação rodando na porta 3001')
+});
+
+// exercício 1
+app.get('/ping', function (_request, response) {
+  return response.status(200).json('pong')
+});
