1cd84dc6bcd7fb92b618f095e54c194e7f9b00f4
andy silva <andssilva.23@gmail.com>
2021-09-05 14:13:48 -0400

    feat: :rocket: content: express rescue

--- bloco_26/dia_5/hello-middleware/errorHandlerExample.js
+++ bloco_26/dia_5/hello-middleware/errorHandlerExample.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const fs = require('fs/promises');
 
 const app = express();
@@ -13,6 +13,26 @@ app.get('/:fileName', async (req, res, next) => {
 });
 
 app.use(function (err, req, res, next) { 
+  res.status(500).json({ error: `Erro: ${err.message}` });
+});
+
+app.listen(3002); */
+
+const express = require('express');
+const rescue = require('express-rescue');
+const fs = require('fs/promises');
+
+const app = express();
+
+app.get(
+  '/:fileName',
+  rescue(async (req, res) => {
+    const file = await fs.readFile(`./${req.params.fileName}`);
+    res.send(file.toString('utf-8'));
+  })
+);
+
+app.use((err, req, res, next) => {
   res.status(500).json({ error: `Erro: ${err.message}` });
 });
 
