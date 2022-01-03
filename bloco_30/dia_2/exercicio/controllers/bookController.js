9f3bf16dfe71b72d3fc569f0829cb6d2f747af7c
Anderson Silva <andssilva.23@gmail.com>
2022-01-03 07:12:13 -0400

    feat: :rocket: exercício: cria a rota POST book/:id

--- bloco_30/dia_2/exercicio/controllers/bookController.js
+++ bloco_30/dia_2/exercicio/controllers/bookController.js
@@ -27,6 +27,25 @@ router.get('/:id', async (req, res) => {
     console.log(err.message);
     res.status(500).json({ message: 'Algo deu errado' });
   }
+});
+
+router.post('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author, pageQuantity = 0 } = req.body;
+
+    const updateBook = await Book.update(
+      { title, author, pageQuantity },
+      { where: { id } },
+    );
+
+    if (!updateBook) return res.status(404).json({ message: 'Livro não encontrado' });
+
+    return res.status(200).json({ message: 'Livro atualizado com sucesso!' });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 })
 
 module.exports = router;
