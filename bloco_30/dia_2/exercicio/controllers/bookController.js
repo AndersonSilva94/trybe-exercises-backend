58517335eb7db48d8c977de1cafc35e06bb47d4f
Anderson Silva <andssilva.23@gmail.com>
2022-01-03 07:01:55 -0400

    feat: :rocket: exercício: cria a rota GET book/:id

--- bloco_30/dia_2/exercicio/controllers/bookController.js
+++ bloco_30/dia_2/exercicio/controllers/bookController.js
@@ -15,4 +15,18 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findByPk(id);
+
+    if (!book) return res.status(404).json({ message: 'Livro não encontrado' });
+
+    return res.status(200).json(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+})
+
 module.exports = router;
